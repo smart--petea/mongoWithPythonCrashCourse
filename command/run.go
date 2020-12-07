17ebb293d7436f266300ff63540ac111a5a367d7
Petru <smart__petea@mail.ru>
2020-12-07 21:25:00 +0200

    53:13 info print

--- command/run.go
+++ command/run.go
@@ -5,6 +5,8 @@ import (
     "log"
     "os"
     "strings"
+    "fmt"
+    "errors"
 
     "tutorial/infrastructure"
 )
@@ -12,40 +14,99 @@ import (
 func Run(env *infrastructure.Environment) {
     commandReader := bufio.NewReader(os.Stdin)
 
+    printLineOfStars()
+
+    fmt.Println()
+    fmt.Println("Welcome to Snake BnB!")
+    fmt.Println("Why are you here?")
+    fmt.Println()
+    fmt.Println("[g] Book a cage for your snake")
+    fmt.Println("[h] Offer extra cage space")
+    fmt.Println()
+    fmt.Println("Are you a [g]uest or [h]ost?")
+
+    cmd, args, err := readCommand(commandReader)
+    if err != nil {
+        log.Fatalln(err)
+    }
+
+    who := ""
+    switch cmd {
+        case "g": 
+            who = "guest"
+        case "h":
+            who = "host"
+        default:
+            log.Fatalln(errors.New("wrong command. Please, start once again."))
+    }
+    fmt.Println(strings.Repeat("*", 19) + " Welcome " + who + " " + strings.Repeat("*", 19))
+    fmt.Println()
+    fmt.Println()
+
+    printInfo()
+
     for {
-        strFullCmd, err := commandReader.ReadString('\n')
+        fmt.Print("> ")
+        cmd, args, err = readCommand(commandReader)
         if err != nil {
             log.Fatalln(err)
         }
 
-        arrFullCmd := strings.Split(strFullCmd, " ")
-        cmd := strings.TrimSpace(arrFullCmd[0])
-
         switch cmd {
-        case "c":
-            CreateAccount(env, arrFullCmd)
-        case "a":
-            CreateAccount(env, arrFullCmd)
-        case "l":
-            LogIntoAccount(env, arrFullCmd)
-        case "y":
-            ListCages(env, arrFullCmd)
-        case "r":
-            RegisterCage(env, arrFullCmd)
-        case "u":
-            UpdateAvailibility(env, arrFullCmd)
-        case "v":
-            ViewBookings(env, arrFullCmd)
-        case "m":
-            ChangeMode(env, arrFullCmd)
+        case "c", "C", "a", "A":
+            CreateAccount(env, args)
+        case "l", "L":
+            LogIntoAccount(env, args)
+        case "y", "Y":
+            ListCages(env, args)
+        case "r", "R":
+            RegisterCage(env, args)
+        case "u", "U":
+            UpdateAvailibility(env, args)
+        case "v", "V":
+            ViewBookings(env, args)
+        case "m", "M":
+            ChangeMode(env, args)
         case "x", "bye", "exit", "exit()":
-            ExitApp(env, arrFullCmd)
+            ExitApp(env, args)
         case "?":
-            ShowCommands(env, arrFullCmd)
+            ShowCommands(env, args)
         case "":
-            None(env, arrFullCmd)
+            None(env, args)
         default:
-            UnknownCommand(env, arrFullCmd)
+            UnknownCommand(env, args)
         }
     }
+}
+
+func printLineOfStars() {
+    fmt.Println(strings.Repeat("*", 50))
+}
+
+func readCommand(commandReader *bufio.Reader) (string, []string, error) {
+        strCmdWithArgs, err := commandReader.ReadString('\n')
+        if err != nil {
+           return "", nil, err
+        }
+
+        cmdWithArgs := strings.Split(strCmdWithArgs, " ")
+        cmd := strings.TrimSpace(cmdWithArgs[0])
+
+        return string(cmd), cmdWithArgs[1:], nil
+} 
+
+func printInfo() {
+    fmt.Println("What action would you like to take:")
+    fmt.Println("[C]reate an [a]ccount")
+    fmt.Println("[L]ogin ton tyour account")
+    fmt.Println("List [y]our cages")
+    fmt.Println("[R]egister a cage")
+    fmt.Println("[U]pdate cage availablity")
+    fmt.Println("[V]iew your bookings")
+    fmt.Println("Change [M]ode (guest or host)")
+    fmt.Println("e[X]it app")
+    fmt.Println("[?] Help (this info)")
+
+    fmt.Println()
+    fmt.Println()
 }
