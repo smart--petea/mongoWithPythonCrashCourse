380b7a950f811a4274774c2897bc1953bf08cf30
Petru <smart__petea@mail.ru>
2020-12-14 21:41:15 +0200

    1:09 validator. bool. false

--- model/cage.go
+++ model/cage.go
@@ -10,17 +10,17 @@ type CreateCageInput struct {
     Name string `validate:"required"`
     Price float32 `validate:"required"`
     SquareMeters float32 `validate:"required"`
-    IsCarpeted bool `validate:"required"`
-    AllowDangerousSnakes bool `validate:"required"`
+    IsCarpeted bool
+    AllowDangerousSnakes bool
 }
 
 type Cage struct {
     ID primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
-    RegisteredDate time.Time `json:"registered_date" bson:"registered_date" validate:"required"`
-    Name string `json:"name" bson:"name" validate:"required"`
-    Price float32 `json:"price" bson:"price" validate:"required"`
-    SquareMeters float32 `json:"square_meters" bson:"square_meters,truncate" validate:"required"`
-    IsCarpeted bool `json:"is_carpeted" bson:"is_carpeted" validate:"required"`
-    AllowDangerousSnakes bool `json:"allow_dangerous_snakes" bson:"allow_dangerous_snakes" validate:"required"`
+    RegisteredDate time.Time `json:"registered_date" bson:"registered_date"`
+    Name string `json:"name" bson:"name"`
+    Price float32 `json:"price" bson:"price"`
+    SquareMeters float32 `json:"square_meters" bson:"square_meters,truncate"`
+    IsCarpeted bool `json:"is_carpeted" bson:"is_carpeted"`
+    AllowDangerousSnakes bool `json:"allow_dangerous_snakes" bson:"allow_dangerous_snakes"`
     Bookings []Booking `json:"bookings" bson:"bookings"`
 }
