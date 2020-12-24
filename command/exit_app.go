e1ad58d018c1ab8f79c8860042056f8be1d7a375
Petru <smart__petea@mail.ru>
2020-12-24 20:43:57 +0200

    1:41:21 exit command

--- command/exit_app.go
+++ command/exit_app.go
@@ -2,6 +2,14 @@ package command
 
 import (
     "tutorial/infrastructure"
+    "tutorial/print"
+
+    "os"
 )
 
-func ExitApp(env *infrastructure.Environment, args []string) {}
+func ExitApp(env *infrastructure.Environment, args []string) {
+    print.Print("\n")
+    print.Success("bye!\n")
+
+    os.Exit(0)
+}
